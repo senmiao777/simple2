b96fca8be76da07a4f5b932f1dd98f897478fa8e
13240411778 <13240411778@163.com>
2020-09-06 15:02:07 +0800

    implements Serializable

--- src/main/java/com/auto/Result.java
+++ src/main/java/com/auto/Result.java
@@ -1,12 +1,15 @@
 package com.auto;
 
+import java.io.Serializable;
+
 /**
  * @author frank
  * @version 1.0
  * @date 2020/9/6 0006 下午 2:43
  * 给前端返回的加密后数据格式
  */
-public class Result {
+public class Result implements Serializable {
+    private static final long serialVersionUID = 4158266974196586397L;
     private Long timeStamp;
     private Object data;
     private String sign;
